9ed442b72c1a41ac7c40df70980efb4321e8cdb2
Gavindu Sandeepa Samarawickrama <gavindusandeepa555@gmail.com>
2026-07-04 10:17:50 +0530

    Refactor customer class code to comments and enhance customer2 object structure

--- js/app.js
+++ js/app.js
@@ -1,23 +1,70 @@
-class Customer{
-    name;
-    age;
-    address;
+// class Customer{
+//     name;
+//     age;
+//     address;
 
-    constructor(name, age, address){
-        this.name = name;
-        this.age = age;
-        this.address = address;
-    }
-}
+//     constructor(name, age, address){
+//         this.name = name;
+//         this.age = age;
+//         this.address = address;
+//     }
+//     getName(){
+//         return this.name;
+//     }
+//     setName(name){
+//         this.name = name;
+//     }
+// }
 
-let customer1 = new Customer("Sharadha", 20, "Bangalore");
-console.log(customer1);
+// let customer1 = new Customer("Sharadha", 20, "Bangalore");
+// console.log(customer1);
+// console.log(customer1.getName());
 
 // ---------------------------------------------
 
-let customer2= {
+let customer2 = {
     name: "John",
     age: 25,
-    address: "New York"
+    address: "New York",
+    salary: 50000,
+    items: [
+        {
+            id: 1,
+            name: "item1",
+            price: 100,
+            variants: [
+                {
+                    id: 1,
+                    name: "variant1",
+                    price: 10
+                },
+                {
+                    id: 2,
+                    name: "variant2",
+                    price: 20
+                    
+                }
+            ]
+        }, {
+            id: 2,
+            name: "item2",
+            price: 200
+        }, {
+            id: 3,
+            name: "item3",
+            price: 300
+        }
+    ],
+    father: {
+        name: "Doe",
+        age: 50,
+        address: "New York"
+    }
 };
-console.log(customer2);
+console.log(customer2.name);
+console.log(customer2.age);
+console.log(customer2.address);
+console.log(customer2.salary);
+console.log(customer2.items[2].name);
+console.log(customer2.father.name);
+console.log(customer2.items[0].variants[1].price);
