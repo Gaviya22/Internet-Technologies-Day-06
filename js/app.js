ab80900e83b64a7d7c86d1201d0ef6f000c4beab
Gavindu Sandeepa Samarawickrama <gavindusandeepa555@gmail.com>
2026-07-04 11:29:43 +0530

    Update heading text in index.html and comment out customer2 object in app.js

--- js/app.js
+++ js/app.js
@@ -22,49 +22,61 @@
 
 // ---------------------------------------------
 
-let customer2 = {
-    name: "John",
-    age: 25,
-    address: "New York",
-    salary: 50000,
-    items: [
-        {
-            id: 1,
-            name: "item1",
-            price: 100,
-            variants: [
-                {
-                    id: 1,
-                    name: "variant1",
-                    price: 10
-                },
-                {
-                    id: 2,
-                    name: "variant2",
-                    price: 20
-                    
-                }
-            ]
-        }, {
-            id: 2,
-            name: "item2",
-            price: 200
-        }, {
-            id: 3,
-            name: "item3",
-            price: 300
-        }
-    ],
-    father: {
-        name: "Doe",
-        age: 50,
-        address: "New York"
-    }
-};
-console.log(customer2.name);
-console.log(customer2.age);
-console.log(customer2.address);
-console.log(customer2.salary);
-console.log(customer2.items[2].name);
-console.log(customer2.father.name);
-console.log(customer2.items[0].variants[1].price);
+// let customer2 = {
+//     name: "John",
+//     age: 25,
+//     address: "New York",
+//     salary: 50000,
+//     items: [
+//         {
+//             id: 1,
+//             name: "item1",
+//             price: 100,
+//             variants: [
+//                 {
+//                     id: 1,
+//                     name: "variant1",
+//                     price: 10
+//                 },
+//                 {
+//                     id: 2,
+//                     name: "variant2",
+//                     price: 20
+
+//                 }
+//             ]
+//         }, {
+//             id: 2,
+//             name: "item2",
+//             price: 200
+//         }, {
+//             id: 3,
+//             name: "item3",
+//             price: 300
+//         }
+//     ],
+//     father: {
+//         name: "Doe",
+//         age: 50,
+//         address: "New York"
+//     }
+// };
+// console.log(customer2.name);
+// console.log(customer2.age);
+// console.log(customer2.address);
+// console.log(customer2.salary);
+// console.log(customer2.items[2].name);
+// console.log(customer2.father.name);
+
+// DOM (Document Object Model) Manipulation -----------------------------------------------
+
+// document.write("<h2>Hello, World! 02</h2>");
+
+console.log(document.title);
+
+
+let heading = document.getElementById("heading");
+
+heading.innerText = "Gavindu";
+
+// console.log(heading.);
