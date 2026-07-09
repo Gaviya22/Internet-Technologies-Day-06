a6e93c43585d2e7f1e6727aa08b5603c559c977c
Gavindu Sandeepa Samarawickrama <gavindusandeepa555@gmail.com>
2026-07-09 16:17:29 +0530

    New Commit

--- js/app.js
+++ js/app.js
@@ -145,7 +145,7 @@ let customerList = [];
 
 function btnAddCustomerOnAction(){
     let txtName = document.getElementById("txtName").value;
-    let txtAddress = document.getElementById("txtAddress").value;
+    let txtAddress = document.getElementById("txtAddress").value
     let txtAge = document.getElementById("txtAge").value;
     let txtEmail = document.getElementById("txtEmail").value;
     let txtSalary = document.getElementById("txtSalary").value;
